9f20c83004065c0da296d86b5a16c95055466954
Manuel Zarat <manuel@zarat.ml>
2025-03-10 14:22:29 +0100

    Update Test-Patch.ps1

--- Test-Patch.ps1
+++ Test-Patch.ps1
@@ -1,4 +1,4 @@
 $token = "test"
 $packages = Get-WingetPackage | where-object { $_.Source -eq 'winget' -and -not ($_.Name -like "Microsoft*" -or $_.Id -like "Microsoft*") } | Select Name
 $data = $((curl https://zarat.lima-city.de/repository.php?token=$token).Content) | ConvertFrom-Json
-$data | where-object { $packages -contains $_.Name }
+$data | where-object { $packages.Name -contains $_.Name }
